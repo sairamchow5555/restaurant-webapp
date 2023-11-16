5b9d4ecf3181356fbc9eee7ac3c72ee047a83f91
Sairam Chundru <sairam.chundru1432@gmail.com>
2023-11-16 12:55:53 +0530

    spellings

--- src/Action/index.js
+++ src/Action/index.js
@@ -1,25 +1,38 @@
 export const settablenumber = (value) => async dispatch =>{
     dispatch({
-        type : "SETTABLENUMBER",
+        type : "SET_TABLE_NUMBER",
         payload : value
     })
 }
 
 export const resettablenumber = () => async dispatch =>{
     dispatch({
-        type : "RESETTABLENUMBER"
+        type : "RESET_TABLE_NUMBER"
     })
 }
 
 export const setfilter = (value) => async dispatch =>{
     dispatch({
-        type : "SETFLTER",
+        type : "SET_FLTER",
         payload : value
     })
 }
 
 export const resetfilter = () => async dispatch =>{
     dispatch({
-        type : "RESETFILTER"
+        type : "RESET_FILTER"
+    })
+}
+
+export const addorder = (id, name, prize, table_number, url) => async dispatch =>{
+    dispatch({
+        type : "ADD_ORDER",
+        payload : {
+            id : id,
+            name : name,
+            prize : prize,
+            table_number : table_number,
+            url : url
+        }
     })
 }
