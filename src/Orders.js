565f1d8db5b8d37fb79f3dc593c03c69af2e75fb
Sairam Chundru <sairam.chundru1432@gmail.com>
2023-11-17 11:15:13 +0530

    oder page display items code

--- src/Orders.js
+++ src/Orders.js
@@ -1,13 +1,43 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import Header from './Container/Header'
 
-const Orders = () => {
+const Orders = ({list}) => {
   return (
     <div>
+      <Header />
         <center>
-            <h4>Orders Component</h4>
-        </center>
+          {list.length > 0 ?
+            <div className='container'>
+              <div className='row'>
+                {list.map((item,id) =>(
+                  <div className='col-md-4' style={{padding:"5px"}} key={item.id}>
+                    <div className='card' style={{width:"18rem",padding:"3px"}}>
+                      <h5 className='card-title'>Table Number: {item.table_number}</h5>
+                      <img src={item.url} className='card-img-top' alt='food item'/>
+                      <div className='card-body'>
+                        <h5 className='card-title'>{item.name}</h5>
+                        <div className='card-text'>
+                          Bill Amount Rs. {item.prize}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+            :
+            <div className='h4'>
+              no order placed at
+            </div>
+          }
+      </center>
     </div>
   )
 }
 
-export default Orders
+const mapStateToProps = state =>({
+  list : state.orderreducer
+})
+
+export default connect(mapStateToProps)(Orders)
